91b73e9d397040c42d7f0a58e3713554358b3155
sanek05077 <sanek05077@gmail.com>
2014-12-19 17:27:52 +0200

    added title for links

--- js/jquery.datepicker.js
+++ js/jquery.datepicker.js
@@ -124,11 +124,11 @@ $(document).ready(function(){
 		
 		
 		$(document).ready(function(){
-			$('#calendar td[title="lightbox-01"]').addClass("lightbox-01");
-			$('#calendar td[title="lightbox-02"]').addClass("lightbox-02");
-			$('#calendar td[title="lightbox-03"]').addClass("lightbox-03");
-			$('#calendar td[title="lightbox-04"]').addClass("lightbox-04");
-			$('#calendar td[title="lightbox-05"]').addClass("lightbox-05");
+			$('#calendar td[abbr="lightbox-01"]').addClass("lightbox-01");
+			$('#calendar td[abbr="lightbox-02"]').addClass("lightbox-02");
+			$('#calendar td[abbr="lightbox-03"]').addClass("lightbox-03");
+			$('#calendar td[abbr="lightbox-04"]').addClass("lightbox-04");
+			$('#calendar td[abbr="lightbox-05"]').addClass("lightbox-05");
 		});
 		
 		
@@ -136,10 +136,10 @@ $(document).ready(function(){
 		
 		
 		$(document).on("click", ".list-years li", function(){
-			$('#calendar td[title="lightbox-01"]').addClass("lightbox-01");
-			$('#calendar td[title="lightbox-02"]').addClass("lightbox-02");
-			$('#calendar td[title="lightbox-03"]').addClass("lightbox-03");
-			$('#calendar td[title="lightbox-04"]').addClass("lightbox-04");
-			$('#calendar td[title="lightbox-05"]').addClass("lightbox-05");
+			$('#calendar td[abbr="lightbox-01"]').addClass("lightbox-01");
+			$('#calendar td[abbr="lightbox-02"]').addClass("lightbox-02");
+			$('#calendar td[abbr="lightbox-03"]').addClass("lightbox-03");
+			$('#calendar td[abbr="lightbox-04"]').addClass("lightbox-04");
+			$('#calendar td[abbr="lightbox-05"]').addClass("lightbox-05");
 		});
 });
